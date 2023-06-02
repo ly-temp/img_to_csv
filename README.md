60383f9e35e85fbfd1a1b9e7864fc806afd380bc
ly-temp <100673133+ly-temp@users.noreply.github.com>
2023-06-02 10:18:45 +0000

    y

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # img_to_csv
+add to testdata: chi_sim.traineddata chi_tra.traineddata eng.traineddata 
+https://github.com/tesseract-ocr/tessdata/raw/main/
---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+#os.environ["TESSDATA_PREFIX"] = os.path.realpath("./testdata")
+
+from img2table.document import Image
+from img2table.ocr import TesseractOCR
+
+ocr = TesseractOCR(lang="eng")
+
+#img = Image(src="/tmp/img/Scan0001.jpg")
+img = Image(src="/tmp/img/test.jpg")
+img_tables = img.extract_tables(ocr=ocr)
+print(img_tables)
+for i in img_tables:
+    #print(f'{i.title} {i.content}')
+    print(i.df)
