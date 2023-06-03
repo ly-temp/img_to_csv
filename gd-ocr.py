c1285e778947da9f16d19a0259f5a3f1be61b912
ly-temp <100673133+ly-temp@users.noreply.github.com>
2023-06-03 08:43:18 +0000

    y

--- gd-ocr.py
+++ gd-ocr.py
@@ -1,14 +1,71 @@
-from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 from google.oauth2 import service_account
 
 def get_service(api_name, api_version, scopes, key_file_location):
-    credentials = service_account.Credentials.from_service_account_file(
-    key_file_location)
-
+    credentials = service_account.Credentials.from_service_account_file(key_file_location)
     scoped_credentials = credentials.with_scopes(scopes)
-
-    # Build the service object.
     service = build(api_name, api_version, credentials=scoped_credentials)
-
     return service
+
+def main():
+    scope = 'https://www.googleapis.com/auth/drive'
+    key_file_location = '/tmp/doc-ocr-388705-d0bfd4c96f1d.json'
+
+
+    try:
+        service = get_service(
+            api_name='drive',
+            api_version='v3',
+            scopes=[scope],
+            key_file_location=key_file_location)
+
+        imgfile = "in.jpg"
+        txtfile = "out.txt"
+
+        root_id = '19R5hikx4bgbgsiC3LvA4bg1GSHIDYlly'
+        results = service.files().list(
+            pageSize=10, fields="nextPageToken, files(id, name)").execute()
+        items = results.get('files', [])
+
+        print(items)
+        for item in items:
+            if item['name'] == 'doc-ocr':
+                root_id = item['id']
+
+        from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
+        import io
+
+        file_metadata = {
+            'name': imgfile,
+            'parents': [root_id],
+        }
+        media = MediaFileUpload('/tmp/Scan0001.jpg',
+                                mimetype='image/png', resumable=True)
+        file_img = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+
+        file_metadata = {
+            'name': txtfile,
+            'parents': [root_id],
+            'mimeType': 'application/vnd.google-apps.document'
+        }
+        file_txt = service.files().copy(fileId=file_img['id'], fields='id', body=file_metadata).execute()
+
+        with open(txtfile, 'wb') as file:
+            request = service.files().export_media(fileId=file_txt['id'], mimeType='text/plain')
+            downloader = MediaIoBaseDownload(file, request)
+            done = False
+            while done is False:
+                status, done = downloader.next_chunk()
+                print(F'Download {int(status.progress() * 100)}.')
+
+
+        service.files().delete(fileId=file_txt['id']).execute()
+        service.files().delete(fileId=file_img['id']).execute()
+
+    except HttpError as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    main()
--- locale.py
+++ locale.py
@@ -1,41 +1,45 @@
-import cv2
+def main():
+    import cv2
 
-img = cv2.imread('/tmp/img/Scan0001.jpg')
-#img = cv2.fastNlMeansDenoising(img, None, 20, 7, 21) 
+    img = cv2.imread('/tmp/img/Scan0001.jpg')
+    #img = cv2.fastNlMeansDenoising(img, None, 20, 7, 21) 
 
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-mask = cv2.inRange(hsv, (100, 10, 130), (170, 255, 255))
-inv_mask = cv2.bitwise_not(mask)
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, (100, 10, 130), (170, 255, 255))
+    inv_mask = cv2.bitwise_not(mask)
 
-#out = img.copy()
-#out[mask!=0] = (255,255,255)
+    #out = img.copy()
+    #out[mask!=0] = (255,255,255)
 
-#edges = cv2.Canny(inv_mask,100,200)
-damaged_image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-out = cv2.inpaint(damaged_image, mask, 0.1, cv2.INPAINT_TELEA)
+    #edges = cv2.Canny(inv_mask,100,200)
+    damaged_image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    out = cv2.inpaint(damaged_image, mask, 0.1, cv2.INPAINT_TELEA)
 
-gray = cv2.cvtColor(out, cv2.COLOR_BGR2GRAY)
-blur = cv2.GaussianBlur(gray, (3,3), 0)
-thresh = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, \
-         cv2.THRESH_BINARY_INV,9,11)
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2,2))
-close = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
-#dilate = cv2.dilate(close, kernel, iterations=1)
-out = cv2.bitwise_not(close)
-#out = 255*(out/255)**5
+    gray = cv2.cvtColor(out, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (3,3), 0)
+    thresh = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, \
+            cv2.THRESH_BINARY_INV,9,11)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2,2))
+    close = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+    #dilate = cv2.dilate(close, kernel, iterations=1)
+    out = cv2.bitwise_not(close)
+    #out = 255*(out/255)**5
 
-cv2.imwrite('out.jpg', out)
+    cv2.imwrite('out.jpg', out)
 
 
-from img2table.document import Image
-from img2table.ocr import TesseractOCR
+    from img2table.document import Image
+    from img2table.ocr import TesseractOCR
 
-ocr = TesseractOCR(lang="eng+chi_tra+chi_sim", psm=3)
+    ocr = TesseractOCR(lang="eng+chi_tra+chi_sim", psm=3)
 
-img = Image(src="/tmp/img/out.jpg")
+    img = Image(src="/tmp/img/out.jpg")
 
-img_tables = img.extract_tables(ocr=ocr)
-for i in img_tables:
-    #print(f'{i.title} {i.content}')
-    print(i.df)
-    #print(i.df.to_csv(f'{i}.csv', sep='\t'))
+    img_tables = img.extract_tables(ocr=ocr)
+    for i in img_tables:
+        #print(f'{i.title} {i.content}')
+        print(i.df)
+        #print(i.df.to_csv(f'{i}.csv', sep='\t'))
+
+if __name__ == "__main__":
+    main()
